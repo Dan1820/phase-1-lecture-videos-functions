c04e2cc2ae81352a2a33d3e63d96d8f04e701b1e
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-15 13:07:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+
+// function sayHi(name){
+//     console.log('hello world '+name)
+// }
+// sayHi('ash')
+
+// const hiRose = function(){
+//     console.log('hi rose')
+
+// }
+// hiRose()
+
+
+const doMaths = function(number,num2){
+    // doMaths(2,5)
+    // return number*num2
+    // console.log(number*num2)
+    return number*num2
+}
+const fiveBy = doMaths(5,5)
+console.log(fiveBy);
+
+// function great(name ,lastname){
+//     console.log('hello ' +name+ ' ' +lastname);
+// }
+// great('james', 'mwangi');
+// great('mary', 'awoi');
+// const name = 'Dan';
+// const programmer= 'pkemoo Dan'
+// function toUpp(text){
+//     const upperCassed= text.toUpperCase();
+   
+//     console.log(upperCassed);
+   
+
+// }
+// toUpp(programmer);
+
+// const greet= (name)=>{
+
+//     console.log('hello ' +name)
+// }
+// greet('mark');
+const salamu = function(name){
+    console.log("habari yako " +name)
+}
+salamu('sherrif')
